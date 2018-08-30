3b7fb8b424c8537f12418da36d14eaa07ad70375
Taiga Abe <taigaabe@dyn-160-39-196-177.dyn.columbia.edu>
2018-08-29 20:34:49 -0400

    added new input pipeline options

--- cgm_train/input.py
+++ cgm_train/input.py
@@ -251,7 +251,53 @@ def temppipeline_2(filenames,base_image):
     base_dataset = tf.data.TFRecordDataset(filenames)
     nb_shards = 6
     index_dataset = tf.data.Dataset.range(nb_shards)
-    mixed = index_dataset.apply(tf.contrib.data.parallel_interleave(lambda x:base_dataset.shard(nb_shards,x).map(preprocess_eff_many).repeat(1).shuffle(1000),cycle_length = 6,block_length = 10,sloppy = True, prefetch_input_elements=1000))
+    mixed = index_dataset.apply(tf.contrib.data.parallel_interleave(lambda x:base_dataset.shard(nb_shards,x).map(preprocess_eff_many).repeat(2).shuffle(1000),cycle_length = 6,block_length = 10,sloppy = True, prefetch_input_elements=1000))
+    # final = mixed.apply(tf.contrib.data.batch_and_drop_remainder(batch_size))
+    final = mixed.batch(batch_size)
+    # Get out the images and tags in a useful format
+    # preprocessed = base_dataset.map(preprocess_p_m).shuffle(50)
+    # ## We now want to batch the dataset into groups of ten neighboring frames:
+    # batches = preprocessed.apply(tf.contrib.data.batch_and_drop_remainder(batch_size))
+    # ## Each of these groups of 10 will be processed together, and their latents will
+    # ## reflect this structure.
+    # ## Shuffle data:
+    # shuffled = batches.repeat(2).shuffle(40)
+    # prefetched = shuffled.prefetch(buffer_size=5)
+    ## Make an iterator:
+
+
+    iterator = tf.data.Iterator.from_structure(final.output_types, final.output_shapes)
+    # iterator = final.make_initializable_iterator()
+    next_images_it,label_it,_,params_it,intensity_it = iterator.get_next()
+    intensity_mult = tf.reshape(intensity_it,shape=(batch_size,1,1,1))
+
+    final_image= tf.maximum(tf.minimum(next_images_it+base_image*intensity_mult/255.,1),0)
+
+    initializer = iterator.make_initializer(final,'init_op')
+    # final_image = next_images_it+base_im*tf.random_uniformuniform/255.
+    ## We finally
+
+    return final_image,label_it,intensity_it,params_it,initializer
+
+def temppipeline_3(filenames,base_image):
+    # filenames = ['datadirectory/toydynamics_nograv/Video_ball_color_small_b'+str(i)+'encoder_train.tfrecords' for i in range(10)]
+
+    # # First try batching by background. In order to do this, we first
+    # # get the tf record filenames into a dataset that can be manipulated
+    # dataset_names = tf.data.Dataset.from_tensor_slices(filenames).shuffle(20)
+    # ## Now we have to define ALL preprocessing that we do prior to
+    # ## interleaving the datasets. This means the preprocessing defined above, as
+    # ## well as shuffling (but before batching).
+    # mixed = dataset_names.apply(tf.contrib.data.parallel_interleave(lambda x:tf.data.TFRecordDataset(x).map(preprocess_p).shuffle(100),cycle_length = 10,block_length=5,sloppy = True, prefetch_input_elements=50))
+    # ## Having cycle and block length equivalent to shuffle length: Think about this.
+    # ## Now we have to batch the mixed dataset and shuffle it:
+    # final = mixed.shuffle(100).apply(tf.contrib.data.batch_and_drop_remainder(batch_size)).repeat(2).shuffle(40)
+    # iterator = final.make_initializable_iterator()
+    # next_images_it,_,_,intensity_it,params_it = iterator.get_next()
+    base_dataset = tf.data.TFRecordDataset(filenames)
+    nb_shards = 6
+    index_dataset = tf.data.Dataset.range(nb_shards)
+    mixed = index_dataset.apply(tf.contrib.data.parallel_interleave(lambda x:base_dataset.shard(nb_shards,x).map(preprocess_eff_many).repeat(1),cycle_length = 5,block_length = 10,sloppy = True, prefetch_input_elements=900))
     # final = mixed.apply(tf.contrib.data.batch_and_drop_remainder(batch_size))
     final = mixed.batch(batch_size)
     # Get out the images and tags in a useful format
